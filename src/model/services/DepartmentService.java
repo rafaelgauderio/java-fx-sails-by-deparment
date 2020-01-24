75065dfbd48466f86d1d2066d04542ff10f01325
Rafael De Luca <deluca1712@gmail.com>
2020-01-24 14:30:50 -0300

    mysql database access

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -3,19 +3,26 @@ package model.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
 public class DepartmentService {
 
-	public List<Department> findAll() {
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1, "Books"));
-		list.add(new Department(2, "Computers"));
-		list.add(new Department(3, "Eletronics"));
-		list.add(new Department(4, "Miscellaneous"));
-		list.add(new Department(5, "Cell phones"));
-		return list;
+	/*
+	 * mock data public List<Department> findAll() { List<Department> list = new
+	 * ArrayList<>(); list.add(new Department(1, "Books")); list.add(new
+	 * Department(2, "Computers")); list.add(new Department(3, "Eletronics"));
+	 * list.add(new Department(4, "Miscellaneous")); list.add(new Department(5,
+	 * "Cell phones")); return list;
+	 * 
+	 * }
+	 */
 
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
+
+	public List<Department> findAll() {
+		return dao.findAll();
 	}
 
 }
